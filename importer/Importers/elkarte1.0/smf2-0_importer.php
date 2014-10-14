386eb53f71dd73eba637ca6db69597cfb4ba0a78
emanuele <emanuele45@gmail.com>
2014-10-14 15:15:18 +0200

    Some functions are better as methods
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- importer/Importers/elkarte1.0/smf2-0_importer.php
+++ importer/Importers/elkarte1.0/smf2-0_importer.php
@@ -718,8 +718,8 @@ class elkarte10_to_smf20
 
 				while ($topicArray = $this->db->fetch_assoc($resultTopic))
 				{
-					$memberStartedID = getMsgMemberID($topicArray['myid_first_msg']);
-					$memberUpdatedID = getMsgMemberID($topicArray['myid_last_msg']);
+					$memberStartedID = $his->getMsgMemberID($topicArray['myid_first_msg']);
+					$memberUpdatedID = $his->getMsgMemberID($topicArray['myid_last_msg']);
 
 					$this->db->query("
 						UPDATE {$to_prefix}topics
@@ -927,7 +927,7 @@ class elkarte10_to_smf20
 						$filename = $custom_avatar_dir . '/' . $row['filename'];
 					}
 					else
-						$filename = getLegacyAttachmentFilename($row['filename'], $row['id_attach']);
+						$filename = $this->getLegacyAttachmentFilename($row['filename'], $row['id_attach']);
 
 					// Probably not one of the imported ones, then?
 					if (!file_exists($filename))
@@ -990,24 +990,53 @@ class elkarte10_to_smf20
 		unset ($_SESSION['import_steps'], $_SESSION['import_progress'], $_SESSION['import_overall']);
 		return true;
 	}
-}
 
-/**
- * helper function for old attachments
- *
- * @param string $filename
- * @param int $attachment_id
- * @return string
- */
-function getLegacyAttachmentFilename($filename, $attachment_id)
-{
-	// Remove special accented characters - ie. sí (because they won't write to the filesystem well.)
-	$clean_name = strtr($filename, 'ŠŽšžŸÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÑÒÓÔÕÖØÙÚÛÜÝàáâãäåçèéêëìíîïñòóôõöøùúûüýÿ', 'SZszYAAAAAACEEEEIIIINOOOOOOUUUUYaaaaaaceeeeiiiinoooooouuuuyy');
-	$clean_name = strtr($clean_name, array('Þ' => 'TH', 'þ' => 'th', 'Ð' => 'DH', 'ð' => 'dh', 'ß' => 'ss', 'Œ' => 'OE', 'œ' => 'oe', 'Æ' => 'AE', 'æ' => 'ae', 'µ' => 'u'));
+	/**
+	 * helper function for old attachments
+	 *
+	 * @param string $filename
+	 * @param int $attachment_id
+	 * @return string
+	 */
+	protected function getLegacyAttachmentFilename($filename, $attachment_id)
+	{
+		// Remove special accented characters - ie. sí (because they won't write to the filesystem well.)
+		$clean_name = strtr($filename, 'ŠŽšžŸÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÑÒÓÔÕÖØÙÚÛÜÝàáâãäåçèéêëìíîïñòóôõöøùúûüýÿ', 'SZszYAAAAAACEEEEIIIINOOOOOOUUUUYaaaaaaceeeeiiiinoooooouuuuyy');
+		$clean_name = strtr($clean_name, array('Þ' => 'TH', 'þ' => 'th', 'Ð' => 'DH', 'ð' => 'dh', 'ß' => 'ss', 'Œ' => 'OE', 'œ' => 'oe', 'Æ' => 'AE', 'æ' => 'ae', 'µ' => 'u'));
 
-	// Get rid of dots, spaces, and other weird characters.
-	$clean_name = preg_replace(array('/\s/', '/[^\w_\.\-]/'), array('_', ''), $clean_name);
-		return $attachment_id . '_' . strtr($clean_name, '.', '_') . md5($clean_name);
+		// Get rid of dots, spaces, and other weird characters.
+		$clean_name = preg_replace(array('/\s/', '/[^\w_\.\-]/'), array('_', ''), $clean_name);
+			return $attachment_id . '_' . strtr($clean_name, '.', '_') . md5($clean_name);
+	}
+
+	/**
+	 *
+	 * Get the id_member associated with the specified message.
+	 * @global type $to_prefix
+	 * @global type $db
+	 * @param type $messageID
+	 * @return int
+	 */
+	protected function getMsgMemberID($messageID)
+	{
+		global $to_prefix, $db;
+
+		// Find the topic and make sure the member still exists.
+		$result = $this->db->query("
+			SELECT IFNULL(mem.id_member, 0)
+			FROM {$to_prefix}messages AS m
+			LEFT JOIN {$to_prefix}members AS mem ON (mem.id_member = m.id_member)
+			WHERE m.id_msg = " . (int) $messageID . "
+			LIMIT 1");
+		if ($this->db->num_rows($result) > 0)
+			list ($memberID) = $this->db->fetch_row($result);
+		// The message doesn't even exist.
+		else
+			$memberID = 0;
+		$this->db->free_result($result);
+
+			return $memberID;
+	}
 }
 
 /**
@@ -1052,33 +1081,4 @@ function copy_smileys($source, $dest)
 		}
 	}
 	closedir($dir);
-}
-
-/**
- *
- * Get the id_member associated with the specified message.
- * @global type $to_prefix
- * @global type $db
- * @param type $messageID
- * @return int
- */
-function getMsgMemberID($messageID)
-{
-	global $to_prefix, $db;
-
-	// Find the topic and make sure the member still exists.
-	$result = $this->db->query("
-		SELECT IFNULL(mem.id_member, 0)
-		FROM {$to_prefix}messages AS m
-		LEFT JOIN {$to_prefix}members AS mem ON (mem.id_member = m.id_member)
-		WHERE m.id_msg = " . (int) $messageID . "
-		LIMIT 1");
-	if ($this->db->num_rows($result) > 0)
-		list ($memberID) = $this->db->fetch_row($result);
-	// The message doesn't even exist.
-	else
-		$memberID = 0;
-	$this->db->free_result($result);
-
-		return $memberID;
 }
