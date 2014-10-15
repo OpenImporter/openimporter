6958b3a74f3d283c2f7128a610a7878ad2da1e14
emanuele <emanuele45@gmail.com>
2014-10-16 00:27:01 +0200

    Server settings belong to the "outside", not to the importer I think
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- importer/import.php
+++ importer/import.php
@@ -17,6 +17,28 @@
 @set_exception_handler(array('ImportException', 'exception_handler'));
 @set_error_handler(array('ImportException', 'error_handler_callback'), E_ALL);
 
+// Clean up after unfriendly php.ini settings.
+if (function_exists('set_magic_quotes_runtime') && version_compare(PHP_VERSION, '5.3.0') < 0)
+	@set_magic_quotes_runtime(0);
+
+error_reporting(E_ALL);
+ignore_user_abort(true);
+umask(0);
+
+ob_start();
+
+// disable gzip compression if possible
+if (is_callable('apache_setenv'))
+	apache_setenv('no-gzip', '1');
+
+if (@ini_get('session.save_handler') == 'user')
+	@ini_set('session.save_handler', 'files');
+@session_start();
+
+// Add slashes, as long as they aren't already being added.
+if (function_exists('get_magic_quotes_gpc') && @get_magic_quotes_gpc() != 0)
+	$_POST = stripslashes_recursive($_POST);
+
 require_once(__DIR__ . '/OpenImporter/SplClassLoader.php');
 $classLoader = new SplClassLoader(null, __DIR__ . '/OpenImporter');
 $classLoader->register();
@@ -191,8 +213,6 @@ class Importer
 		$this->template = $template;
 		$this->headers = $headers;
 
-		$this->_cleanupServerSettings();
-
 		$this->_findScript();
 
 		// The current step - starts at 0.
@@ -248,34 +268,6 @@ class Importer
 	protected function saveInSession()
 	{
 		$_SESSION['importer_data'] = $this->data;
-	}
-
-	/**
-	 * Some serevr settings require more work to be fit for the purpose
-	 */
-	protected function _cleanupServerSettings()
-	{
-		// Clean up after unfriendly php.ini settings.
-		if (function_exists('set_magic_quotes_runtime') && version_compare(PHP_VERSION, '5.3.0') < 0)
-			@set_magic_quotes_runtime(0);
-
-		error_reporting(E_ALL);
-		ignore_user_abort(true);
-		umask(0);
-
-		ob_start();
-
-		// disable gzip compression if possible
-		if (is_callable('apache_setenv'))
-			apache_setenv('no-gzip', '1');
-
-		if (@ini_get('session.save_handler') == 'user')
-			@ini_set('session.save_handler', 'files');
-		@session_start();
-
-		// Add slashes, as long as they aren't already being added.
-		if (function_exists('get_magic_quotes_gpc') && @get_magic_quotes_gpc() != 0)
-			$_POST = stripslashes_recursive($_POST);
 	}
 
 	/**
