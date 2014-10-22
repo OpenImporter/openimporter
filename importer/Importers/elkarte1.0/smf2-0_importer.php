61b8c44dd82cfc09e9c51d1c8c3c09a20362ea8d
emanuele <emanuele45@gmail.com>
2014-10-22 13:53:13 +0200

    Improved SMF 2.0 attachments importing to ElkArte 1.0
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- importer/Importers/elkarte1.0/smf2-0_importer.php
+++ importer/Importers/elkarte1.0/smf2-0_importer.php
@@ -41,3 +41,33 @@ class SMF2_0
 		return 'members';
 	}
 }
+
+function moveAttachment($row, $db, $from_prefix, $attachmentUploadDir)
+{
+	static $smf_folders = null;
+
+	if ($smf_folders === null
+	{
+		$request = $db->query("
+			SELECT value
+			FROM {$from_prefix}settings
+			WHERE variable='attachmentUploadDir';");
+		list ($smf_attachments_dir) = $db->fetch_row($request);
+
+		$smf_folders = @unserialize($smf_attachments_dir);
+		if (!is_array($smf_folders))
+			$smf_folders = array(1 => $smf_attachments_dir);
+	}
+
+	$smf_attachments_dir = $smf_folders[$row['id_folder']];
+
+	if (empty($row['file_hash']))
+	{
+		$row['file_hash'] = createAttachmentFileHash($row['filename']);
+		$source_file = $row['filename'];
+	}
+	else
+		$source_file = $row['id_attach'] . '_' . $row['file_hash'];
+
+	copy_file($smf_attachments_dir . '/' . $source_file, $attachmentUploadDir . '/' . $row['id_attach'] . '_' . $row['file_hash'] . '.elk');
+}
