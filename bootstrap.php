f341e427f1dec5cc2ca822db5d2e927b0c78e4c7
emanuele <emanuele45@gmail.com>
2014-10-25 01:35:47 +0200

    Update phpunit bootstrap to include the definition of BASEDIR
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- bootstrap.php
+++ bootstrap.php
@@ -3,3 +3,5 @@
 require_once(__DIR__ . '/importer/OpenImporter/SplClassLoader.php');
 $classLoader = new SplClassLoader(null, __DIR__ . '/importer/OpenImporter');
 $classLoader->register();
+
+define('BASEDIR', __DIR__ . '/importer');
