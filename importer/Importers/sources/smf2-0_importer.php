3499f58d2cb6cee4f4b0dea964ed95fff0897b99
emanuele <emanuele45@gmail.com>
2014-12-29 23:13:19 +0100

    And let's conclude with SMF 2.0 importer
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- importer/Importers/sources/smf2-0_importer.php
+++ importer/Importers/sources/smf2-0_importer.php
@@ -171,6 +171,244 @@ class SMF2_0 extends AbstractSourceImporter
 
 		return $rows;
 	}
+
+	public function codeSettings()
+	{
+		// @todo this list looks broken (I don't remember any enablePinnedTopics in SMF 1.1)
+		$do_import = array(
+			'news',
+			'compactTopicPagesContiguous',
+			'compactTopicPagesEnable',
+			'enablePinnedTopics',
+			'todayMod',
+			'enablePreviousNext',
+			'pollMode',
+			'enableVBStyleLogin',
+			'enableCompressedOutput',
+			'attachmentSizeLimit',
+			'attachmentPostLimit',
+			'attachmentNumPerPostLimit',
+			'attachmentDirSizeLimit',
+			'attachmentExtensions',
+			'attachmentCheckExtensions',
+			'attachmentShowImages',
+			'attachmentEnable',
+			'attachmentEncryptFilenames',
+			'attachmentThumbnails',
+			'attachmentThumbWidth',
+			'attachmentThumbHeight',
+			'censorIgnoreCase',
+			'mostOnline',
+			'mostOnlineToday',
+			'mostDate',
+			'allow_disableAnnounce',
+			'trackStats',
+			'userLanguage',
+			'titlesEnable',
+			'topicSummaryPosts',
+			'enableErrorLogging',
+			'max_image_width',
+			'max_image_height',
+			'onlineEnable',
+			'smtp_host',
+			'smtp_port',
+			'smtp_username',
+			'smtp_password',
+			'mail_type',
+			'timeLoadPageEnable',
+			'totalMembers',
+			'totalTopics',
+			'totalMessages',
+			'simpleSearch',
+			'censor_vulgar',
+			'censor_proper',
+			'enablePostHTML',
+			'enableEmbeddedFlash',
+			'xmlnews_enable',
+			'xmlnews_maxlen',
+			'hotTopicPosts',
+			'hotTopicVeryPosts',
+			'registration_method',
+			'send_validation_onChange',
+			'send_welcomeEmail',
+			'allow_editDisplayName',
+			'allow_hideOnline',
+			'guest_hideContacts',
+			'spamWaitTime',
+			'pm_spam_settings',
+			'reserveWord',
+			'reserveCase',
+			'reserveUser',
+			'reserveName',
+			'reserveNames',
+			'autoLinkUrls',
+			'banLastUpdated',
+			'avatar_max_height_external',
+			'avatar_max_width_external',
+			'avatar_action_too_large',
+			'avatar_max_height_upload',
+			'avatar_max_width_upload',
+			'avatar_resize_upload',
+			'avatar_download_png',
+			'failed_login_threshold',
+			'oldTopicDays',
+			'edit_wait_time',
+			'edit_disable_time',
+			'autoFixDatabase',
+			'allow_guestAccess',
+			'time_format',
+			'number_format',
+			'enableBBC',
+			'max_messageLength',
+			'signature_settings',
+			'autoOptMaxOnline',
+			'defaultMaxMessages',
+			'defaultMaxTopics',
+			'defaultMaxMembers',
+			'enableParticipation',
+			'recycle_enable',
+			'recycle_board',
+			'maxMsgID',
+			'enableAllMessages',
+			'fixLongWords',
+			'who_enabled',
+			'time_offset',
+			'cookieTime',
+			'lastActive',
+			'requireAgreement',
+			'unapprovedMembers',
+			'package_make_backups',
+			'databaseSession_enable',
+			'databaseSession_loose',
+			'databaseSession_lifetime',
+			'search_cache_size',
+			'search_results_per_page',
+			'search_weight_frequency',
+			'search_weight_age',
+			'search_weight_length',
+			'search_weight_subject',
+			'search_weight_first_message',
+			'search_max_results',
+			'search_floodcontrol_time',
+			'permission_enable_deny',
+			'permission_enable_postgroups',
+			'mail_next_send',
+			'mail_recent',
+			'settings_updated',
+			'next_task_time',
+			'warning_settings',
+			'admin_features',
+			'last_mod_report_action',
+			'pruningOptions',
+			'cache_enable',
+			'reg_verification',
+			'enable_buddylist',
+			'birthday_email',
+			'globalCookies',
+			'default_timezone',
+			'memberlist_updated',
+			'latestMember',
+			'latestRealName',
+			'db_mysql_group_by_fix',
+			'rand_seed',
+			'mostOnlineUpdated',
+			'search_pointer',
+			'spider_name_cache',
+			'modlog_enabled',
+			'disabledBBC',
+			'latest_member',
+			'latest_real_name',
+			'total_members',
+			'total_messages',
+			'max_msg_id',
+			'total_topics',
+			'disable_hash_time',
+			'latestreal_name',
+			'disableHashTime',
+		);
+
+		$request = $this->db->query("
+			SELECT variable, value
+			FROM {$this->config->from_prefix}settings;");
+
+		$rows = array();
+		while ($row = $this->db->fetch_assoc($request))
+		{
+			if (in_array($row['variable'], $do_import))
+			{
+				$rows[] = array(
+					'variable' => $row['variable'],
+					'value' => $row['value'],
+				);
+			}
+		}
+		$this->db->free_result($request);
+
+		return $rows;
+	}
+
+	public function codeAvatars()
+	{
+		$avatarg = $this->db->query("
+			SELECT value
+			FROM {$this->config->from_prefix}settings
+			WHERE variable = 'avatar_directory';");
+		list ($smf_avatarg) = $this->db->fetch_row($avatarg);
+		$this->db->free_result($avatarg);
+
+		$avatar_gallery = array();
+		if (!empty($smf_avatarg) && file_exists($smf_avatarg))
+			$avatar_gallery = get_files_recursive($smf_avatarg);
+
+		$avatarg = $this->db->query("
+			SELECT value
+			FROM {$this->config->from_prefix}settings
+			WHERE variable = 'custom_avatar_dir';");
+		list ($smf_avatarg) = $this->db->fetch_row($avatarg);
+		$this->db->free_result($avatarg);
+
+		$avatar_custom = array();
+		if (!empty($smf_avatarg) && file_exists($smf_avatarg))
+			$avatar_custom = get_files_recursive($smf_avatarg);
+
+		return array_merge($avatar_gallery, $avatar_custom);
+	}
+
+	public function codeCopysmiley()
+	{
+		$request = $this->db->query("
+			SELECT value
+			FROM {$this->config->from_prefix}settings
+			WHERE variable = 'smileys_dir';");
+		list ($smf_smileys_dir) = $this->db->fetch_row($request);
+
+		if (!empty($smf_smileys_dir) && file_exists($smf_smileys_dir))
+		{
+			$smf_smileys_dir = str_repeat('\\', '/', $smf_smileys_dir);
+			$smiley = array();
+			$files = get_files_recursive($smf_smileys_dir);
+			foreach ($files as $file)
+			{
+				$file = str_repeat('\\', '/', $file);
+				$smiley[] = array(
+					'basedir' => $smf_smileys_dir,
+					'full_path' => dirname($file),
+					'filename' => basename($file),
+				);
+			}
+			if (!empty($smiley))
+				return array($smiley);
+			else
+				return false;
+		}
+		else
+			return false;
+	}
+
+	public function codeLikes()
+	{
+		return $this->fetchLikes();
+	}
 }
 
 function moveAttachment($row, $db, $from_prefix, $attachmentUploadDir)
