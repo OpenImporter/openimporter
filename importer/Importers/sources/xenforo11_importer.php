9a77c9a852a06575acd7cf3da9ff5c273ff1edd7
emanuele <emanuele45@gmail.com>
2014-12-29 14:18:04 +0100

    And XenForo 1.1 as well
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- importer/Importers/sources/xenforo11_importer.php
+++ importer/Importers/sources/xenforo11_importer.php
@@ -39,4 +39,53 @@ class XenForo1_1 extends AbstractSourceImporter
 	{
 		return 'user';
 	}
+
+	/**
+	 * From here on, all the methods are needed helper for the conversion
+	 */
+	public function preparseMembers($originalRows)
+	{
+		$rows = array();
+		foreach ($originalRows as $row)
+		{
+			$pass = unserialize($row['tmp']);
+
+			if (isset($pass['hash']))
+				$row['passwd'] = $pass['hash'];
+			else
+				$row['passwd'] = sha1(md5(mktime()));
+	
+			if	(isset($pass['salt']))
+				$row['password_salt'] = $pass['salt'];
+			else
+				$row['password_salt'] = '';
+			unset($row['tmp']);
+
+			$rows[] = $row;
+		}
+
+		return $rows;
+	}
+
+	public function preparseBoards($originalRows)
+	{
+		$rows = array();
+		foreach ($originalRows as $row)
+		{
+			$request = $this->db->query("
+				SELECT 
+					thread_id, last_post_id
+				FROM {$from_prefix}thread
+				WHERE node_id  = $row[id_board]
+				ORDER BY thread_id DESC
+				LIMIT 1");
+
+				list($tmp, $row['id_last_msg']) = $this->db->fetch_row($request);
+				$this->db->free_result($request);
+
+			$rows[] = $row;
+		}
+
+		return $rows;
+	}
 }
