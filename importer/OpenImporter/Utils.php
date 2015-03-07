a255d9ff6689868b1b834776483efa10849cf1fd
emanuele <emanuele45@gmail.com>
2015-03-07 13:29:19 +0100

    And a version bumb since the development branch is holding what has been identified as version 2.0
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- importer/OpenImporter/Utils.php
+++ importer/OpenImporter/Utils.php
@@ -4,7 +4,7 @@
  * @copyright OpenImporter contributors
  * @license   BSD http://opensource.org/licenses/BSD-3-Clause
  *
- * @version 1.0 Alpha
+ * @version 2.0 Alpha
  *
  * This file contains code based on:
  *
