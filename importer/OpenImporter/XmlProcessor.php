6128cb33f641caad5580672a55b07ecea6107bb4
emanuele <emanuele45@gmail.com>
2015-03-26 23:43:55 +0100

    Fix use of <options><ignore>
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- importer/OpenImporter/XmlProcessor.php
+++ importer/OpenImporter/XmlProcessor.php
@@ -399,7 +399,7 @@ class XmlProcessor
 		if (isset($options->ignore) && $options->ignore == false)
 			return false;
 
-		return !isset($options->ignore) && !isset($options->replace);
+		return isset($options->ignore) && !isset($options->replace);
 	}
 
 	protected function shouldReplace($options)
