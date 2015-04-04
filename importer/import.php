feac9398c2ae0ada3e37b3888df4f0718cc2c575
emanuele <emanuele45@gmail.com>
2015-04-04 17:12:06 +0200

    We are already php 5.3+ so no reason to keep deprecated code
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- importer/import.php
+++ importer/import.php
@@ -32,10 +32,6 @@ $loader->register();
 @set_exception_handler(array('ImportException', 'exception_handler'));
 @set_error_handler(array('ImportException', 'error_handler_callback'), E_ALL);
 
-// Clean up after unfriendly php.ini settings.
-if (function_exists('set_magic_quotes_runtime') && version_compare(PHP_VERSION, '5.3.0') < 0)
-	@set_magic_quotes_runtime(0);
-
 error_reporting(E_ALL);
 ignore_user_abort(true);
 umask(0);
