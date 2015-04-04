f6a0296d22f1cd39713299ddf7ce07328ccd283b
emanuele <emanuele45@gmail.com>
2015-04-04 16:01:17 +0200

    Forgot a file
    Signed-off-by: emanuele <emanuele45@gmail.com>

---
+++ importer/OpenImporter/StepException.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @name      OpenImporter
+ * @copyright OpenImporter contributors
+ * @license   BSD http://opensource.org/licenses/BSD-3-Clause
+ *
+ * @version 2.0 Alpha
+ */
+
+namespace OpenImporter\Core;
+
+/**
+ * class DatabaseException extends the build-in Exception class and
+ * catches potential errors
+ */
+class StepException extends \Exception
+{
+	protected $template;
+
+	public function __construct($template)
+	{
+		$this->template = $template;
+	}
+
+	public function doExit()
+	{
+		$this->template->footer();
+	}
+}
