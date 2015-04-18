5a9c11ffa91a9fb19fd9d44324bc32a59188f84c
emanuele <emanuele45@gmail.com>
2015-04-18 13:51:01 +0200

    Adding tests for the Files class
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- importer/OpenImporter/Files.php
+++ importer/OpenImporter/Files.php
@@ -5,12 +5,6 @@
  * @license   BSD http://opensource.org/licenses/BSD-3-Clause
  *
  * @version 2.0 Alpha
- *
- * This file contains code based on:
- *
- * Simple Machines Forum (SMF)
- * copyright:	2011 Simple Machines (http://www.simplemachines.org)
- * license:	BSD, See included LICENSE.TXT for terms and conditions.
  */
 
 namespace OpenImporter\Core;
