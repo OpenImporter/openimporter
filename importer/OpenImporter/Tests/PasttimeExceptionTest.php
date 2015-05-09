674071eb9edaf77a42636227b2a59b6d03f7a7f3
emanuele <emanuele45@gmail.com>
2015-05-09 19:14:59 +0200

    Tests for PasttimeException
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- importer/OpenImporter/Tests/PasttimeExceptionTest.php
+++ importer/OpenImporter/Tests/PasttimeExceptionTest.php
@@ -10,10 +10,40 @@
 namespace OpenImporter\Importers\sources\Tests;
 
 use OpenImporter\Core\PasttimeException;
+use OpenImporter\Core\Configurator;
+use OpenImporter\Core\Lang;
+use OpenImporter\Core\Template;
 
 class PasttimeExceptionTest extends \PHPUnit_Framework_TestCase
 {
-	public function testNothing()
+	public function testDoExit()
 	{
+		$template = new DummyTemplatePasttimeExceptionTest();
+
+		$instance = new PasttimeException($template, 'bar', 'import_progress', 'max', 'step', 'start');
+		$instance->doExit();
+
+		$this->assertEquals(array('bar', 'import_progress', 'max', 'step', 'start'), $template->called_timeLimit);
+		$this->assertTrue($template->called_footer);
+	}
+}
+
+class DummyTemplatePasttimeExceptionTest extends Template
+{
+	public $called_timeLimit = false;
+	public $called_footer = false;
+
+	public function __construct()
+	{
+	}
+
+	public function timeLimit($bar, $import_progress, $max, $step, $start)
+	{
+		$this->called_timeLimit = array($bar, $import_progress, $max, $step, $start);
+	}
+
+	public function footer($inner = true)
+	{
+		$this->called_footer = true;
 	}
 }
