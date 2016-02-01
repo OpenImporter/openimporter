eefc2a5fda5e9ceb4aeb006a660ad333184b5df0
spuds <spuds@spudsdesign.com>
2016-02-01 17:22:35 -0600

    ! Set the autoloader to the namespaces ! initiate those namesapces ! misc comments / formatting

--- importer/import.php
+++ importer/import.php
@@ -7,72 +7,91 @@
  * @version 1.0 Alpha
  */
 
+// Handy shortcut
 define('BASEDIR', __DIR__);
 
+// Autoload our classes from the OpenImporter and Importers directory's
 require_once(BASEDIR . '/OpenImporter/SplClassLoader.php');
-$classLoader = new SplClassLoader(null, BASEDIR . '/OpenImporter');
+$classLoader = new SplClassLoader('OpenImporter', BASEDIR);
 $classLoader->register();
-$classLoader2 = new SplClassLoader(null, BASEDIR . '/Importers');
+$classLoader2 = new SplClassLoader('Importers', BASEDIR);
 $classLoader2->register();
 
+// Can always ask, but whats taking 10mins?
 @set_time_limit(600);
-@set_exception_handler(array('ImportException', 'exception_handler'));
-@set_error_handler(array('ImportException', 'error_handler_callback'), E_ALL);
+
+// Lets catch those errors and exceptions
+error_reporting(E_ALL);
+set_exception_handler(array('OpenImporter\ImportException', 'exception_handler'));
+set_error_handler(array('OpenImporter\ImportException', 'error_handler_callback'), E_ALL);
 
 // Clean up after unfriendly php.ini settings.
 if (function_exists('set_magic_quotes_runtime') && version_compare(PHP_VERSION, '5.3.0') < 0)
+{
 	@set_magic_quotes_runtime(0);
+}
 
-error_reporting(E_ALL);
+// User aborts are not a good thing
 ignore_user_abort(true);
+
+// Let try to create files as 0666 and directories as 0777.
 umask(0);
 
-ob_start();
-
-// disable gzip compression if possible
+// Disable gzip compression if possible
 if (is_callable('apache_setenv'))
+{
 	apache_setenv('no-gzip', '1');
+}
 
-if (@ini_get('session.save_handler') == 'user')
+if (@ini_get('session.save_handler') === 'user')
+{
 	@ini_set('session.save_handler', 'files');
+}
+
+ob_start();
 @session_start();
+global $import;
 
 // Add slashes, as long as they aren't already being added.
 if (function_exists('get_magic_quotes_gpc') && @get_magic_quotes_gpc() != 0)
+{
 	$_POST = stripslashes_recursive($_POST);
+}
 
-$config = new Configurator();
+// Start a configurator values container for use
+$config = new OpenImporter\Configurator();
 $config->lang_dir = BASEDIR . '/Languages';
 
+// Load our language strings, can't say much without them
 try
 {
-	$lng = new Lang();
+	// Load the users language based on detected browser language settings
+	$lng = new OpenImporter\Lang();
 	$lng->loadLang($config->lang_dir);
 }
 catch (Exception $e)
 {
-	ImportException::exception_handler($e);
+	OpenImporter\ImportException::exception_handler($e);
 }
 
-$template = new Template($lng);
-
-global $import;
-$importer = new Importer($config, $lng, $template);
-$response = new HttpResponse(new ResponseHeader());
+// Template, import and response engine
+$template = new OpenImporter\Template($lng);
+$importer = new OpenImporter\Importer($config, $lng, $template);
+$response = new OpenImporter\HttpResponse(new OpenImporter\ResponseHeader());
 
 $template->setResponse($response);
+$import = new OpenImporter\ImportManager($config, $importer, $template, new OpenImporter\Cookie(), $response);
 
-$import = new ImportManager($config, $importer, $template, new Cookie(), $response);
-
+// Lets get this show on the road
 try
 {
 	$import->process();
 }
 catch (Exception $e)
 {
-	// Debug, remember to remove before PR
+	// Debug, remember to remove before GA
 	echo '<br>' . $e->getMessage() . '<br>';
 	echo $e->getFile() . '<br>';
 	echo $e->getLine() . '<br>';
-	// If an error is not catched, it means it's fatal and the script should die.
+	// If an error is not caught, it means it's fatal and the script should die.
 }
