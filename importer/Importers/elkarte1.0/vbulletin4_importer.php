941effcf5d24b9cdab14aec0feb8b2286dc5e22e
spuds <spuds@spudsdesign.com>
2016-02-02 10:50:51 -0600

    ! more conffgi => oi_config changes

--- importer/Importers/elkarte1.0/vbulletin4_importer.php
+++ importer/Importers/elkarte1.0/vbulletin4_importer.php
@@ -7,7 +7,7 @@
  * @version 1.0 Alpha
  */
 
-class vBulletin_4 extends AbstractSourceImporter
+class vBulletin_4 extends Importers\AbstractSourceImporter
 {
 	protected $setting_file = '/includes/config.php';
 
@@ -23,16 +23,16 @@ class vBulletin_4 extends AbstractSourceImporter
 
 	public function getPrefix()
 	{
-		global $config;
+		global $oi_config;
 
-		return '`' . $this->getDbName() . '`.' . $config['Database']['tableprefix'];
+		return '`' . $this->getDbName() . '`.' . $oi_config['Database']['tableprefix'];
 	}
 
 	public function getDbName()
 	{
-		global $config;
+		global $oi_config;
 
-		return $config['Database']['dbname'];
+		return $oi_config['Database']['dbname'];
 	}
 
 	public function getTableTest()
@@ -43,6 +43,10 @@ class vBulletin_4 extends AbstractSourceImporter
 
 /**
  * Utility functions
+ *
+ * @param string $content
+ *
+ * @return mixed|string
  */
 function vb4_replace_bbc($content)
 {
@@ -62,12 +66,13 @@ function vb4_replace_bbc($content)
 			'[list type=decimal]',
 		), strtr($content, array('"' => '&quot;')));
 
-	// fixing Code tags
+	// Fixing Code tags
 	$replace = array();
-	
+
 	preg_match('~\[code\](.+?)\[/code\]~is', $content, $matches);
 	foreach ($matches as $temp)
 		$replace[$temp] = htmlspecialchars($temp);
+
 	$content = substr(strtr($content, $replace), 0, 65534);
 
 	return $content;
